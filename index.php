e8104d951bdd63f076d3f97d7751e292c54a7354
KostiukVM <kostiukdsfv@gmail.com>
2023-12-26 22:51:20 +0200

    test Form2

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 <div class="conteiner">
 
 
-    <form method="post" action="index.php" width="200px" height="200px">
+    <form method="post" action="index.php" >
 
         <?php
         var_dump($_POST);
@@ -38,10 +38,10 @@
             <label for="exampleInputEmail1" class="form-label" >Email address</label>
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
         </div>
-        <!--        <div class="mb-3">-->
-        <!--            <label for="exampleInputPassword1" class="form-label">Password</label>-->
-        <!--            <input type="password" class="form-control" id="exampleInputPassword1">-->
-        <!--        </div>-->
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
         <br>
